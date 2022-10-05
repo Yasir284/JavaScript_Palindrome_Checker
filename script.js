f1e26376340931cc3d1b18e1e19e755c04c0f07e
Yasir284 <lambawalay@gmail.com>
2022-10-05 20:02:36 +0530

    change

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 // Palindrome Checker Game:
 //    Users can enter text or numbers and check whether the entered value is palindrome or not by clicking on the check button.
+
+let text = document.querySelector("#text");
+
+function check() {
+  let txt = text.value
+    .split("")
+    .map((val) => val.toLowerCase().trim())
+    .filter((val) => val != "" && val != " " && val != ".");
+  console.log(txt);
+
+  if (txt.join("") == txt.reverse().join("")) {
+    return alert("yes");
+  } else {
+    return alert("no");
+  }
+}
+
+function clearText() {
+  text.value = "";
+}
